acf9775b23c82cbcf3ec940170b2cfefa4bf177d
Steve Mu <qmu@bu.edu>
2019-11-09 16:52:54 -0500

    add redux persist

--- client/src/App.js
+++ client/src/App.js
@@ -2,44 +2,55 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-
-import reducers from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { PersistGate } from "redux-persist/integration/react";
 
 import Home from "./pages/Home";
 import Offer from "./pages/Offer";
 import Request from "./pages/Request";
+import rootReducer from "./reducers";
 
-const store = createStore(reducers);
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/offer">Offer</Link>
-            </li>
-            <li>
-              <Link to="/request">Rquest</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/offer">
-            <Offer />
-          </Route>
-          <Route path="/request">
-            <Request />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/offer">Offer</Link>
+              </li>
+              <li>
+                <Link to="/request">Rquest</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/offer">
+              <Offer />
+            </Route>
+            <Route path="/request">
+              <Request />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
