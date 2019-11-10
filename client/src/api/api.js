4c6f7829ddad8da2247306a0f6b5aef3fa700fa2
Steve Mu <qmu@bu.edu>
2019-11-10 01:09:59 -0500

    api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -7,49 +7,42 @@ export async function get(url, token) {
   if (!res.ok) {
     throw "error for " + url;
   }
-  console.log(res);
+
   let j = await res.json();
-  console.log(j);
   return j;
 }
 
-// export async function postRequest(url, body) {
-//   const { getTokenSilently } = useAuth0();
-//   const token = await getTokenSilently();
+export async function post(url, body, token) {
+  let res = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${token}`
+    }
+  });
+  if (!res.ok) {
+    throw "error for " + url;
+  }
+  let json = await res.json();
+  return json;
+}
 
-//   let res = await fetch(url, {
-//     method: "POST",
-//     body: JSON.stringify(body),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8",
-//       Authorization: `Bearer ${token}`
-//     }
-//   });
-//   if (!res.ok) {
-//     throw "error for " + url;
-//   }
-//   let json = await res.json();
-//   return json;
-// }
-
-// export async function deleteRequest(url, body = {}) {
-//   const { getTokenSilently } = useAuth0();
-//   const token = await getTokenSilently();
-
-//   let res = await fetch(url, {
-//     method: "DELETE",
-//     body: JSON.stringify(body),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8",
-//       Authorization: `Bearer ${token}`
-//     }
-//   });
-//   if (!res.ok) {
-//     throw "error for " + url;
-//   }
-//   let json = await res.json();
-//   return json;
-// }
+export async function del(url, body = {}, token) {
+  let res = await fetch(url, {
+    method: "DELETE",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${token}`
+    }
+  });
+  if (!res.ok) {
+    throw "error for " + url;
+  }
+  let json = await res.json();
+  return json;
+}
 
 // export async function getOffers() {
 //   return [
@@ -66,6 +59,5 @@ export async function get(url, token) {
 
 export async function getRequests(token) {
   let json = await get('/requests', token);
-  console.log(json);
   return json;
 }
