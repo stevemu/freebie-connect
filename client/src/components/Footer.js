287cbfd05dcb2d9230643e0df27f6f25fc37c335
Jing L <jingl@Jings-MacBook-Pro.local>
2019-11-10 01:05:53 -0500

    footer.js v3

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,24 +5,7 @@ import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 function Footer() {
-<<<<<<< HEAD
-  return (
-    <div className="footer">
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <NavLink to="/contact">Contact Us</NavLink>
-            <NavLink to="/Support">Support</NavLink>
-          </Nav>
-          &copy;{new Date().getFullYear()}
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
-=======
     return (
-        
       <div class="fixed-bottom">
         <div className="footer">
           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -38,6 +21,5 @@ function Footer() {
         </div>
       </div>
     );
->>>>>>> cb8f3005fd4a887c881ea1170ad772564d0c245d
 }
 export default Footer;
