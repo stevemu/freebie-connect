ee772030cf18da64027f4e4e4c283657e9790173
Steve Mu <qmu@bu.edu>
2019-11-10 05:56:21 -0500

    solve loading problem

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,23 +12,40 @@ function Home(props) {
   const [requests, setRequests] = useState([]);
   const [offers, setOffers] = useState([]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  } else {
-    async function fetchRequests() {
-      let token = await getTokenSilently();
-      let requests = await getRequests(token);
-      setRequests(requests);
-    }
-    // fetchRequests();
-    async function fetchOffers() {
-      let token = await getTokenSilently();
-      let offers = await getOffers(token);
-      setOffers(offers);
-    }
-    // fetchOffers();
+  useEffect(() => {
+    if (loading) return;
+    fetchRequests();
+    fetchOffers();
+  }, [loading]);
 
+  async function fetchRequests() {
+    let token = await getTokenSilently();
+    let requests = await getRequests(token);
+    setRequests(requests);
   }
+
+  async function fetchOffers() {
+    let token = await getTokenSilently();
+    let offers = await getOffers(token);
+    setOffers(offers);
+  }
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // } else {
+  //   async function fetchRequests() {
+  //     let token = await getTokenSilently();
+  //     let requests = await getRequests(token);
+  //     setRequests(requests);
+  //   }
+  //   // fetchRequests();
+  //   async function fetchOffers() {
+  //     let token = await getTokenSilently();
+  //     let offers = await getOffers(token);
+  //     setOffers(offers);
+  //   }
+  //   // fetchOffers();
+
+  // }
 
   const homeStyle = {
     margin: "0 25%"
