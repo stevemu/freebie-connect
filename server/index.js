da93e3eba40e60ac39e4bd268dc564339fede3d8
Steve Mu <qmu@bu.edu>
2019-11-09 22:04:20 -0500

    send 404 if not authorized

--- server/index.js
+++ server/index.js
@@ -2,13 +2,36 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
-const {configRoutes} = require('./routes');
+var jwt = require("express-jwt");
+var jwks = require("jwks-rsa");
+
+const { configRoutes } = require("./routes");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+let port = 7500;
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://stevemu.auth0.com/.well-known/jwks.json"
+  }),
+  audience: "https://freebie-connect-api.stevemu.com",
+  issuer: "https://stevemu.auth0.com/",
+  algorithms: ["RS256"]
+});
+
+app.use(jwtCheck);
+
 configRoutes(app);
 
-let port = 7500;
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({});
+  }
+});
 
 switch (process.env.NODE_ENV) {
   case "production":
--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,14 @@
-
-
 function configRoutes(app) {
-
   app.post("/test", async (req, res) => {
     res.json({});
   });
 
+  app.get("/authorized", function(req, res) {
+    console.log(req.user);
+    // if (!req.user.admin) return res.sendStatus(401);
 
+    res.send("Secured Resource");
+  });
 }
 
 exports.configRoutes = configRoutes;
