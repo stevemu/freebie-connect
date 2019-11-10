35a0305891e8e83364c2a067880b2da13c83e456
Steve Mu <qmu@bu.edu>
2019-11-10 01:43:31 -0500

    show requests from db

---
+++ client/src/components/ItemCard.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import hammer from "../images/shovel.jpg";
+
+function ItemCard(props) {
+  let { title, desc, borderStyle } = props;
+  return (
+    <Card border={borderStyle}>
+      {/* <Card.Header>Category</Card.Header> */}
+      <Card.Img variant="top" src={hammer} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{desc}</Card.Text>
+        <Button variant="primary">Respond</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ItemCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,15 +5,15 @@ import { updateAuthToken } from "../actions/auth";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
 import Button from "react-bootstrap/Button";
-// import ListGroup from "react-bootstrap/ListGroup";
 import test from "../images/test.png";
-import test1 from "../images/shovel.jpg";
+import hammer from "../images/shovel.jpg";
 import test2 from "../images/hammer.jpg";
 
 import ListGroup from "react-bootstrap/ListGroup";
 import { useAuth0 } from "../react-auth0-spa";
 import { getRequests } from "../api/api";
 
+import ItemCard from "../components/ItemCard";
 
 function Home(props) {
   const { loading, getTokenSilently } = useAuth0();
@@ -32,7 +32,6 @@ function Home(props) {
     init();
   }
 
-
   const homeStyle = {
     margin: "0 25%"
   };
@@ -43,7 +42,7 @@ function Home(props) {
         <div className="row">
           <div className="col-sm-6 col-sm-6">
             <CardGroup>
-              <Card border="success" >
+              <Card border="success">
                 {/* <Card.Header>Category</Card.Header> */}
                 <Card.Img variant="top" src={test} />
                 <Card.Body>
@@ -52,16 +51,16 @@ function Home(props) {
                   <Button variant="primary">Go Get It</Button>
                 </Card.Body>
               </Card>
-              <Card border="success" >
+              <Card border="success">
                 {/* <Card.Header>Category</Card.Header> */}
-                <Card.Img variant="top" src={test1} />
+                <Card.Img variant="top" src={hammer} />
                 <Card.Body>
                   <Card.Title> Offering Shovel </Card.Title>
                   <Card.Text>Got Shovel for you to use</Card.Text>
                   <Button variant="primary">Go Get It</Button>
                 </Card.Body>
               </Card>
-              <Card border="success" >
+              <Card border="success">
                 {/* <Card.Header>Category</Card.Header> */}
                 <Card.Img variant="top" src={test2} />
                 <Card.Body>
@@ -75,33 +74,9 @@ function Home(props) {
           <div className="col-sm-6 col-sm-6">
             <h1>Requests</h1>
             <CardGroup>
-              <Card border="danger" >
-                {/* <Card.Header>Category</Card.Header> */}
-                <Card.Img variant="top" src={test} />
-                <Card.Body>
-                  <Card.Title> Need Skiing Gears </Card.Title>
-                  <Card.Text>Need to borrow skiing gears</Card.Text>
-                  <Button variant="primary">Respond</Button>
-                </Card.Body>
-              </Card>
-              <Card border="danger">
-                {/* <Card.Header>Category</Card.Header> */}
-                <Card.Img variant="top" src={test2} />
-                <Card.Body>
-                  <Card.Title> Need Hammer </Card.Title>
-                  <Card.Text>Need to borrow a Hammer</Card.Text>
-                  <Button variant="primary">Respond</Button>
-                </Card.Body>
-              </Card>
-              <Card border="danger" >
-                {/* <Card.Header>Category</Card.Header> */}
-                <Card.Img variant="top" src={test1} />
-                <Card.Body>
-                  <Card.Title> Need a Shovel </Card.Title>
-                  <Card.Text>Need to borrow a shovel</Card.Text>
-                  <Button variant="primary">Respond</Button>
-                </Card.Body>
-              </Card>
+              {r.map(r => {
+                return <ItemCard key={r._id} {...r} borderStyle="danger" />;
+              })}
             </CardGroup>
           </div>
         </div>
